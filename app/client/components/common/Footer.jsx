fd40f20e297a3d6e071f6ca84106de0343adcfee
Damir <damir@withpulp.com>
2015-08-29 05:53:10 -0400

    update to footer component

--- app/client/components/common/Footer.jsx
+++ app/client/components/common/Footer.jsx
@@ -1,13 +1,13 @@
 C.Footer = React.createClass({
     render() {
         return (
-            <div className="footer container">
-                <div className="footer-content">
-
-                </div>
-                <div className="footer-footer">
-
-                </div>
+            <div className="footer">
+                  <p className="copyright">
+                       &copy;2015,
+                       <a className="creator" href="http://damirvazgird.com" target="_blank">
+                            Damir Vazgird
+                       </a>
+                  </p>
             </div>
         )
     }

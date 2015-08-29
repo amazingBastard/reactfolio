6d8f0b6a56b963836a1b612dcc6f1de63d7c575e
Damir <damir@withpulp.com>
2015-08-29 15:23:26 -0400

    modifed new project form

--- app/client/components/forms/login/AdminLogin.jsx
+++ app/client/components/forms/login/AdminLogin.jsx
@@ -1,41 +1,35 @@
 App.AdminLogin = React.createClass({
     mixins: [],
-    PropTypes: {
-
-    },
+    PropTypes: {},
     getInitialState() {
         return {
             errors: {}
         }
     },
     getMeteorData() {
-        return {
-
-        }
+        return {}
     },
     onSubmit(event) {
         event.preventDefault();
 
-        var self = this;
-
-        var email = $(event.target).find('[name=email]').val();
-        var password = $(event.target).find('[name=password]').val();
-
-        var errors = {};
+        var self = this,
+            email = $(event.target).find('[name=email]').val(),
+            password = $(event.target).find('[name=password]').val(),
+            errors = {};
 
         if (!email) {
-            errors.email = 'Email required'
+            errors.email = 'Valid email is required'
         }
 
         if (!password) {
-            errors.password = 'Password required'
+            errors.password = 'Password is required'
         }
 
         self.setState({
             errors: errors
         });
 
-        if (! _.isEmpty(errors)) {
+        if (!_.isEmpty(errors)) {
             return;
         }
 
@@ -55,11 +49,15 @@ App.AdminLogin = React.createClass({
         return (
             <div className="admin login form module">
                 <h1 className="title"><i className="fa fa-terminal"></i>Admin Login</h1>
+
                 <form className="admin login form" onSubmit={this.onSubmit}>
-                    <App.AuthErrors errors={this.state.errors} />
-                    <App.FormInput hasError={!!this.state.errors.email} label="none" icon="fa fa-envelope" name="Email" type="email" placeholder="email@email.com" />
-                    <App.FormInput hasError={!!this.state.errors.password} label="none" icon="fa fa-unlock" name="Password" type="password" placeholder="password" />
-                    <button type="submit" className="primary fluid submit login button"><i className="fa fa-hand-peace-o"></i></button>
+                    <App.AuthErrors errors={this.state.errors}/>
+                    <App.FormInput hasError={!!this.state.errors.email} label="none" icon="fa fa-envelope" name="Email"
+                                   type="email" placeholder="email@email.com"/>
+                    <App.FormInput hasError={!!this.state.errors.password} label="none" icon="fa fa-unlock"
+                                   name="Password" type="password" placeholder="password"/>
+                    <button type="submit" className="primary fluid submit login button"><i
+                        className="fa fa-hand-peace-o"></i></button>
                 </form>
             </div>
         )
--- app/client/components/forms/project/InsertProject.jsx
+++ app/client/components/forms/project/InsertProject.jsx
@@ -1,25 +1,62 @@
 App.InsertProject = React.createClass({
     mixins: [],
     PropTypes: {},
+    getInitialState() {
+        return {
+            errors: {}
+        }
+    },
+    getMeteorData() {
+        return {}
+    },
     onSubmit(event) {
         event.preventDefault();
 
-        // Find the fields via the React ref
-        var title = React.findDOMNode(this.refs.title).value.trim(),
-            image = React.findDOMNode(this.refs.image).value.trim(),
-            content = React.findDOMNode(this.refs.content).value.trim();
+        var self = this,
+            title = $(event.target).find('[name=title]').val(),
+            image = $(event.target).find('[name=image]').val(),
+            content = $(event.target).find('[name=content]').val(),
+            errors = {};
 
-        Projects.insert({
-            title: title,
-            image: image,
-            dontent: content,
-            createdAt: new Date()
+        if (!title) {
+            errors.title = 'Title is required'
+        }
+
+        if (!image) {
+            errors.image = 'Image url is required'
+        }
+
+        if (!content) {
+            errors.content = 'Content is required'
+        }
+
+        self.setState({
+            errors: errors
         });
 
-        // Clear form
-        React.findDOMNode(this.refs.title).value = '';
-        React.findDOMNode(this.refs.image).value = '';
-        React.findDOMNode(this.refs.content).value = '';
+        if (!_.isEmpty(errors)) {
+            return;
+        }
+
+        if (err) {
+            self.setState({
+                errors: {'none': err.reason}
+            });
+
+            return
+        } else {
+            Projects.insert({
+                title: title,
+                image: image,
+                dontent: content,
+                createdAt: new Date()
+            });
+
+            // Clear form
+            React.findDOMNode(this.refs.title).value = '';
+            React.findDOMNode(this.refs.image).value = '';
+            React.findDOMNode(this.refs.content).value = '';
+        }
     },
     render() {
         return (
@@ -27,10 +64,15 @@ App.InsertProject = React.createClass({
                 <h1 className="title"><i className="fa fa-terminal"></i>New Project</h1>
 
                 <form className="insert project form" onSubmit={this.onSubmit}>
-                    <input type="text" ref="title" placeholder="Title"/>
-                    <input type="text" ref="image" placeholder="Image"/>
-                    <textarea type="text" ref="content" placeholder="Content"></textarea>
-                    <input type="submit" className="primary submit project button"/>
+                    <App.AuthErrors errors={this.state.errors}/>
+                    <App.FormInput hasError={!!this.state.errors.title} label="none" icon="fa fa-envelope" name="Title"
+                                   type="text" placeholder="project title"/>
+                    <App.FormInput hasError={!!this.state.errors.image} label="none" icon="fa fa-unlock" name="Image"
+                                   type="text" placeholder="image url"/>
+                    <App.FormInput hasError={!!this.state.errors.content} label="none" icon="fa fa-unlock"
+                                   name="Content" type="textarea" placeholder="content"/>
+                    <button type="submit" className="primary fluid submit project button"><i
+                        className="fa fa-hand-peace-o"></i></button>
                 </form>
             </div>
         )

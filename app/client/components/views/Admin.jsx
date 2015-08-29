14fa2bb1dbaca85a15e23410047a7f8380461685
Damir <damir@withpulp.com>
2015-08-29 10:36:49 -0400

    added forms to admin view

--- app/client/components/views/Admin.jsx
+++ app/client/components/views/Admin.jsx
@@ -1,8 +1,19 @@
 C.Admin = React.createClass({
+    // This mixin makes the getMeteorData method work
+    mixins: [ReactMeteorData],
+
+    // Loads items from the Projects collection and puts them on this.data.projects
+    getMeteorData() {
+         return {
+              projects: Projects.find({}, {sort: {createdAt: -1}}).fetch(),
+              currentUser: Meteor.user()
+         }
+    },
     render() {
          return (
               <main className="admin view">
-
+                    <C.UserLogin />
+                    <C.InsertProject / >
               </main>
          )
     }

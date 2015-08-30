6da6910b2bf7b89da28f0875c2c536642401e891
Damir <damir@withpulp.com>
2015-08-30 19:19:10 -0400

    loader replaced with gif

--- app/client/components/common/Loading.jsx
+++ app/client/components/common/Loading.jsx
@@ -2,24 +2,7 @@ App.Loading = React.createClass({
     render() {
         return (
             <div className="loading module">
-                <div className="space">
-  	                <div className="jetpack">
-      	                <span className="jet"></span>
-      	                <span className="jet"></span>
-                        <span className="jet"></span>
-                        <span className="jet"></span>
-                    </div>
-                    <div className="rocketman">
-                        <span className="limbs"></span>
-                        <div className="helmet"></div>
-                    </div>
-                </div>
-                <div className="streaks">
-                    <span className="streak"></span>
-                    <span className="streak"></span>
-                    <span className="streak"></span>
-                    <span className="streak"></span>
-                </div>
+                <img className="loader image" src="loader.gif" />
             </div>
         )
     }

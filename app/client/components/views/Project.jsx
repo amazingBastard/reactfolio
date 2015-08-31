b41953cd7726a330b3ca4545cebc8b3b7a6c668c
Damir <damir@withpulp.com>
2015-08-31 19:50:19 -0400

    small markup update to project

--- app/client/components/views/Project.jsx
+++ app/client/components/views/Project.jsx
@@ -12,9 +12,8 @@ App.Project = React.createClass({
     },
     renderProject() {
         return <article className="project">
-                   <a href="/">Back</a>
                    <h1 className="title">{this.data.project.title}</h1>
-                   <img className="image" src={this.data.project.image} />
+                   <img className="fluid image" src={this.data.project.image} />
                    <div className="content">{this.data.project.content}</div>
                </article>;
     },

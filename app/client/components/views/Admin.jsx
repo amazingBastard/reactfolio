560077d2f17e6081d57bed1c0d93f481b30d6d43
Damir <damir@withpulp.com>
2015-09-04 00:07:37 -0400

    fixed currentUser condition in admin

--- app/client/components/views/Admin.jsx
+++ app/client/components/views/Admin.jsx
@@ -7,14 +7,10 @@ App.Admin = React.createClass({
 
         if (handle.ready()) {
             data.projects = Projects.find({}, {sort: {createdAt: -1}}).fetch();
+            data.currentUser = Meteor.user();
         }
 
         return data;
-
-        //return {
-        //    data,
-        //    currentUser: Meteor.user()
-        //};
     },
 
     removeThisProject(event) {
@@ -26,47 +22,30 @@ App.Admin = React.createClass({
     renderProjects() {
         return this.data.projects.map(function (project) {
             var path = FlowRouter.path('ProjectEdit', {_id: project._id});
-            return <li className="project" key={project._id}>
-                       <a className="path" href={path}>
-                            <h1 className="title">{project.title}</h1>
-                       </a>
-                       <button type="button" className="remove project icon button" onClick={this.removeThisProject}><i className="fa fa-ban"></i></button>
-                   </li>;
+            return (
+                <li className="project" key={project._id}>
+                    <a className="path" href={path}>
+                        <h1 className="title">{project.title}</h1>
+                    </a>
+                    <button type="button" className="remove project icon button" onClick={this.removeThisProject}><i
+                        className="fa fa-ban"></i></button>
+                </li>
+            )
         });
     },
 
     render() {
-        return (
-            <main className="animated fadeIn admin view">
+        if (this.data.currentUser) {
+            return <main className="animated fadeIn admin view">
                 <ul className="projects module">
                     {(this.data.projects) ? this.renderProjects() : <App.Loading />}
                 </ul>
-                <a className="fluid primary new project button" href="/admin/new">New Project</a>
+                < a className="fluid primary new project button" href="/admin/new">New Project</a>
             </main>
-        )
-
-        /*
-        let dashboard;
-        let { currentUser } = this.data.currentUser;
-
-        if (currentUser) {
-            dashboard = (
-                <div className="projects module">
-                    {(this.data.projects) ? this.renderProjects() : <App.Loading />}
-                </div>
-                //<App.InsertProject />
-            )
         } else {
-            dashboard = (
+            return <main className="animated fadeIn admin view">
                 <App.AdminLogin />
-            )
-        }
-
-        return (
-            <main className="animated fadeIn admin view">
-                {dashboard}
             </main>
-        )
-        */
+        }
     }
 });

10ba3bdfad1b3aef2bae2a80033365a12b02c969
Damir <damir@withpulp.com>
2015-09-05 19:23:01 -0400

    update project component merged into project edit

--- app/client/components/views/ProjectEdit.jsx
+++ app/client/components/views/ProjectEdit.jsx
@@ -1,6 +1,11 @@
 App.ProjectEdit = React.createClass({
     mixins: [ReactMeteorData],
     PropTypes: {},
+    getInitialState() {
+        return {
+            errors: {}
+        }
+    },
     getMeteorData() {
         var data = {},
             handle = Meteor.subscribe('project', this.props._id);
@@ -11,10 +16,79 @@ App.ProjectEdit = React.createClass({
 
         return data;
     },
+    onSubmit(event) {
+        event.preventDefault();
+
+        var self = this,
+            title = $(event.target).find('[name=title]').val(),
+            image = $(event.target).find('[name=image]').val(),
+            description = $(event.target).find('[name=description]').val(),
+            content = $(event.target).find('[name=content]').val(),
+            errors = {};
+
+        if (!title) {
+            errors.title = 'Title is required'
+        }
+
+        if (!image) {
+            errors.image = 'Image url is required'
+        }
+
+        if (!description) {
+            errors.description = 'Description is required'
+        }
+
+        if (!content) {
+            errors.content = 'Content is required'
+        }
+
+        self.setState({
+            errors: errors
+        });
+
+        if (!_.isEmpty(errors)) {
+            return;
+        }
+
+        Meteor.call('insertProject', title, image, description, content, (err) => {
+            if (err) {
+                self.setState({
+                    errors: {'none': err.reason}
+                });
+
+                return;
+            } else {
+                // Reset form
+                document.querySelector('form').reset();
+            }
+        });
+
+    },
+    renderUpdateProject() {
+        return (
+            <div className="update form module">
+                <h1 className="title"><i className="fa fa-terminal"></i>Edit Project</h1>
+
+                <form className="update form" onSubmit={this.onSubmit}>
+                    <App.AuthErrors errors={this.state.errors}/>
+                    <App.FormInput hasError={!!this.state.errors.title} label="icon" icon="fa fa-book" name="Title"
+                                   type="text" placeholder="project title" value={this.data.project.title}/>
+                    <App.FormInput hasError={!!this.state.errors.image} label="icon" icon="fa fa-picture-o" name="Image"
+                                   type="text" placeholder="image url" value={this.data.project.image}/>
+                    <App.FormInput hasError={!!this.state.errors.description} label="icon" icon="fa fa-pencil"
+                                   name="Description" type="textarea" placeholder="Project description" value={this.data.project.description}/>
+                    <App.FormInput hasError={!!this.state.errors.content} label="icon" icon="fa fa-pencil"
+                                   name="Content" type="textarea" placeholder="Write about your project" value={this.data.project.content}/>
+                    <button type="submit" className="primary fluid update button"><i
+                        className="fa fa-floppy-o"></i></button>
+                </form>
+            </div>
+        )
+    },
     render() {
         return (
             <main className="animated fadeIn admin edit project view">
-                {(this.data.project) ? <App.UpdateProject /> : <App.Loading />}
+                {(this.data.project) ? this.renderUpdateProject() : <App.Loading />}
             </main>
         )
     }

672d7b75ac8d19837bb8d0b2d871814198318d70
Damir <damir@withpulp.com>
2015-09-05 21:16:30 -0400

    permissions created

--- app/collections/client/projects.js
+++ app/collections/client/projects.js
@@ -1,1 +1,13 @@
 Projects = new Mongo.Collection('projects');
+
+Projects.allow({
+    update: ownsDocument,
+    remove: ownsDocument
+});
+
+Projects.deny({
+    update: function(userId, post, fieldNames) {
+        // may only edit the following fields:
+        return (_.without(fieldNames, 'title', 'image', 'description', 'content').length > 0);
+    }
+});

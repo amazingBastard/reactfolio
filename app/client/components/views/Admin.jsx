b64a8d88d845b9813c96d5fb2f457326e42601cc
Damir <damir@withpulp.com>
2015-12-04 23:06:59 -0500

    code refactoring wip

--- app/client/components/views/Admin.jsx
+++ app/client/components/views/Admin.jsx
@@ -1,58 +1,34 @@
 App.Admin = React.createClass({
     mixins: [ReactMeteorData],
-    PropTypes: {},
+
     getMeteorData() {
-        var data = {},
-            selector = {username: 'admin'},
-            handle = Meteor.subscribe('projects', selector);
+        let user = Meteor.user(),
+            selector = {username: user.username},
+            subscription = Meteor.subscribe('projects', selector);
 
-        if (handle.ready()) {
-            data.projects = Projects.find({}, {sort: {createdAt: -1}}).fetch();
-            data.currentUser = Meteor.user();
-            data.userLoading = Meteor.loggingIn();
-        }
-
-        return data;
-    },
-
-    removeThisProject() {
-        Meteor.call('removeProject', this.props.project._id);
-    },
-
-    renderProjects() {
-        return this.data.projects.map(function (project) {
-            var path = FlowRouter.path('ProjectUpdate', {_id: project._id});
-            return (
-                <li className="project" key={project._id}>
-                    <a className="path" href={path}>
-                        <h1 className="title">{project.title}</h1>
-                    </a>
-                    <button type="button" className="remove project icon button" onClick={this.removeThisProject}><i
-                        className="fa fa-ban"></i></button>
-                </li>
-            )
-        });
+        return {
+            isLoading: !subscription.ready(),
+            games: Projects.find({}, {sort: {createdAt: -1}}).fetch()
+        };
     },
 
     render() {
-        if (this.data.userLoading) {
-            return <App.Loading />
-        }
-        if (this.data.currentUser) {
-            return (
-                <main className="animated fadeIn admin view">
-                    <ul className="projects module">
-                        {(this.data.projects) ? this.renderProjects() : <App.Loading />}
-                    </ul>
-                    < a className="fluid primary new project button" href="/admin/new">New Project</a>
-                </main>
-            )
+        let noProjects = this.data.projects.length === 0,
+            messageProps = {
+                message: 'There are no projects',
+                class: 'centered'
+            };
+
+        if (this.data.isLoading) {
+            return <App.Loading />;
         } else {
             return (
                 <main className="animated fadeIn admin view">
-                    <App.AdminLogin />
+                    {noProjects ? <App.Messages messageProps={messageProps} /> :
+                        <App.Projects projects={this.data.projects}/>}
+                    <a className="fluid primary new project button" href="/admin/new">New Project</a>
                 </main>
-            )
+            );
         }
     }
 });

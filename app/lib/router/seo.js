e15544017854dc288ee275816c65b3696945634f
Damir <damir@withpulp.com>
2015-12-05 20:12:46 -0500

    user perms... router config and seo

---
+++ app/lib/router/seo.js
@@ -0,0 +1,12 @@
+SEO = new FlowRouterSEO();
+
+SEO.setDefaults({
+    title: 'Reactfolio | A Design Portfolio built with React and Meteor',
+    description: 'A minimal portfolio for graphic or ui/ux designers. Create or edit projects in markdown through the live editor. You can also manage content through admin dashboard.',
+    meta: {
+        'property="og:type"': 'webApp',
+        'property="og:site_name"': 'reactfolio',
+        'name="twitter:card"': 'Reactfolio',
+        'name="twitter:site"': '@dvazgird'
+    }
+});

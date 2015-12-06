0310c6af1581c317cf9da0e628a51950f1da4daa
Damir <damir@withpulp.com>
2015-12-06 00:10:50 -0500

    created login and dashboard views - wip

---
+++ app/client/components/views/private/dashboard.jsx
@@ -0,0 +1,9 @@
+App.Dashboard = React.createClass({
+    render() {
+        return (
+            <view className="animated fadeIn dashboard view">
+                <p>dashboard view</p>
+            </view>
+        );
+    }
+});
---
+++ app/client/components/views/public/login.jsx
@@ -0,0 +1,9 @@
+App.Login = Reacct.createClass({
+    render() {
+        return (
+            <view className="animated fadeIn login view">
+                <h1 className="title">Login</h1>
+            </view>
+        );
+    }
+});

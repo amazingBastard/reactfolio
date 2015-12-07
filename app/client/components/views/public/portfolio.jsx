d8421c2168c8763d24dd1db3ae96e72f8388ba93
Damir <damir@withpulp.com>
2015-12-07 00:41:07 -0500

    portfolio view

---
+++ app/client/components/views/public/portfolio.jsx
@@ -0,0 +1,37 @@
+App.Portfolio = React.createClass({
+    mixins: [ReactMeteorData],
+
+    getMeteorData() {
+        let selector = {author: 'Damir Vazgird'},
+            subscription = Meteor.subscribe('projects', selector);
+
+        return {
+            isLoading: !subscription.ready(),
+            projects: Projects.find({}, {sort: {created: -1}}).fetch()
+        };
+    },
+
+    shouldComponentUpdate() {
+        return true;
+    },
+
+    render() {
+        let projects = this.data.projects,
+            messageProps = {
+                module: 'messages module',
+                type: 'centered message',
+                message: 'There are no projects'
+            };
+
+        if (this.data.isLoading) {
+            return <App.Loading />;
+        } else {
+            return (
+                <view className="animated fadeIn root view">
+                    {!projects ? <App.Messages messageProps={messageProps} /> :
+                        <App.Projects projects={projects}/>}
+                </view>
+            );
+        }
+    }
+});

b792b3fc96ae1457103092c2ff94a3a000376cfb
Damir <damir@withpulp.com>
2015-12-06 23:37:22 -0500

    update to logo module

--- app/client/components/modules/common/logo.jsx
+++ app/client/components/modules/common/logo.jsx
@@ -1,23 +1,11 @@
 App.Logo = React.createClass({
-    mixins: [ReactMeteorData],
-    getMeteorData() {
-        return {
-            currentUser: Meteor.user()
-        }
-    },
-    handleLogout() {
-        Meteor.logout();
-    },
     render() {
         return (
             <module className="animated fadeInDown logo module">
-                <h1 className="title">Reactfolio</h1>
-                <h2 className="title">Reactfolio</h2>
-                <h3 className="title">Reactfolio</h3>
-                <h4 className="title">Reactfolio</h4>
-                <h5 className="title">Reactfolio</h5>
-                <h6 className="title">Reactfolio</h6>
+                <a className="logo" href="/">
+                    <h1 className="title">Reactfolio</h1>
+                </a>
             </module>
-        )
+        );
     }
 });

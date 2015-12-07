bf6818c56728d97e04031fc47ae07f4ea3a6555d
Damir <damir@withpulp.com>
2015-12-06 22:08:50 -0500

    typography scales

--- app/client/components/modules/common/logo.jsx
+++ app/client/components/modules/common/logo.jsx
@@ -9,29 +9,14 @@ App.Logo = React.createClass({
         Meteor.logout();
     },
     render() {
-        let buttons;
-        let { currentUser } = this.data;
-
-        if (currentUser) {
-            buttons = (
-                <div className="buttons">
-                    <a className="admin icon button" href="/dashboard"><i className="fa fa-user"></i></a>
-                    <button className="logout icon button" onClick={this.handleLogout}><i className="fa fa-sign-out"></i>
-                    </button>
-                </div>
-            )
-        } else {
-            buttons = (
-                <div className="buttons">
-                    <a className="login icon button" href="/login"><i className="fa fa-sign-in"></i></a>
-                </div>
-            )
-        }
-
         return (
             <module className="animated fadeInDown logo module">
-                <h1 className="title"><a className="route" href="/"><span className="brand font">React</span>folio</a></h1>
-                {buttons}
+                <h1 className="title">Reactfolio</h1>
+                <h2 className="title">Reactfolio</h2>
+                <h3 className="title">Reactfolio</h3>
+                <h4 className="title">Reactfolio</h4>
+                <h5 className="title">Reactfolio</h5>
+                <h6 className="title">Reactfolio</h6>
             </module>
         )
     }

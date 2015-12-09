fe3c9bcc235e3abd7bee498f6d15e8c4e1214ba1
Damir <damir@withpulp.com>
2015-12-09 02:55:09 -0500

    project form updates

--- app/client/components/elements/forms/project/project_form.jsx
+++ app/client/components/elements/forms/project/project_form.jsx
@@ -1,17 +1,37 @@
 App.ProjectForm = React.createClass({
-    PropTypes: {},
+    propTypes: {
+        project: React.PropTypes.object
+    },
 
     render() {
-        return (
-            <form className="project form">
-                <div className="input group">
-                    <label className="icon label" htmlFor="title"><i className="fa fa-file"></i></label>
-                    <input type="text" name="title" className="title input" placeholder="Project title" autofocus />
-                </div>
-                <div className="input group">
-                    <textarea name="content" className="content input" placeholder="Write about your project..."></textarea>
-                </div>
-            </form>
-        );
+        if (this.props.project) {
+            let title = this.props.project.title,
+                content = this.props.project.content;
+
+            return (
+                <form className="update project form">
+                    <div className="input group">
+                        <label className="icon label" htmlFor="title"><i className="fa fa-file"></i></label>
+                        <input type="text" name="title" className="title input" placeholder="Project title"
+                               value={title} autofocus/>
+                    </div>
+                    <div className="edit markdown">
+                        <textarea name="content" className="content input" placeholder="Write about your project...">{content}</textarea>
+                    </div>
+                </form>
+            );
+        } else {
+            return (
+                <form className="insert project form">
+                    <div className="input group">
+                        <label className="icon label" htmlFor="title"><i className="fa fa-file"></i></label>
+                        <input type="text" name="title" className="title input" placeholder="Project title" autofocus/>
+                    </div>
+                    <div className="edit markdown">
+                        <textarea name="content" className="content input" placeholder="Write about your project..."></textarea>
+                    </div>
+                </form>
+            );
+        }
     }
 });

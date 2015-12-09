6d90f191e6ece966c8a60a9418411e10de56c8bf
Damir <damir@withpulp.com>
2015-12-09 01:19:41 -0500

    toolbar in insert view

--- app/client/components/views/private/insert.jsx
+++ app/client/components/views/private/insert.jsx
@@ -1,4 +1,25 @@
 App.Insert = React.createClass({
+    renderToolbar() {
+        // @TODO: toggle buttons when state of the editor changes
+        // preview/edit on left
+        // save draft/publish/cancel on the right
+
+        let leftButton = {
+                type: 'left icon button',
+                icon: 'fa fa-eye'
+            }, rightButton = {
+                type: 'right icon button',
+                icon: 'fa fa-save'
+            };
+
+        return (
+            <module className="toolbar module">
+                <App.Button buttonProps={leftButton}/>
+                <App.Button buttonProps={rightButton}/>
+            </module>
+        );
+    },
+
     render() {
         return (
             <view className="animated fadeIn insert view">
@@ -6,6 +27,7 @@ App.Insert = React.createClass({
                 <p>title with markdown editor form</p>
                 <p>toggle preview/edit mode action</p>
                 <p>save draft and publish actions</p>
+                {this.renderToolbar()}
             </view>
         );
     }

5d1c50c3b43c97f0865ccd2ea4e69c1216dfcbc2
Damir <damir@withpulp.com>
2015-12-13 12:30:21 -0800

    added currentUser helper to dashboard

--- app/client/components/views/private/dashboard.jsx
+++ app/client/components/views/private/dashboard.jsx
@@ -1,8 +1,22 @@
 App.Dashboard = React.createClass({
+    mixins: [ReactMeteorData],
+
+    getMeteorData() {
+        return {
+            user: Meteor.user()
+        };
+    },
+
+    shouldComponentUpdate() {
+        return true;
+    },
+
     render() {
+        let currentUser = this.data.user.profile.name;
+
         return (
             <view className="animated fadeIn dashboard view">
-                <p>dashboard view</p>
+                <p className="greeting">Hello, {currentUser}!</p>
             </view>
         );
     }

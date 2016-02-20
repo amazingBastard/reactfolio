7553f9b3ee2fafba0cc6f6bb63b343b49d3b76d4
Damir <damir@withpulp.com>
2016-02-20 13:54:53 -0500

    added marked options to markdown component

--- app/client/components/modules/markdown/markdown.jsx
+++ app/client/components/modules/markdown/markdown.jsx
@@ -10,7 +10,17 @@ App.Markdown = React.createClass({
     renderHTML() {
         let content = this.props.content;
 
-        return {__html: marked(content, {sanitize: true})};
+        return {
+            __html: marked.parse(content, {
+                gfm: true,
+                tables: true,
+                breaks: false,
+                pedantic: false,
+                sanitize: false,
+                smartLists: true,
+                smartypants: false
+            })
+        };
     },
 
     render() {
